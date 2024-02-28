ba6e00c59e36e1411f7887909c8e93525b02740d
PatriciaMach <157374755+PatriciaMach@users.noreply.github.com>
2024-02-27 22:15:46 -0300

    Update app.js peso  e altura

--- js-curso-2-desafio_1/app.js
+++ js-curso-2-desafio_1/app.js
@@ -1,7 +1,7 @@
 function calcularIMC(alt,kg,) {
    let imc = kg / (alt . alt);
 }
-let kg = 58;
-let alt = 180;
+let kg = 69;
+let alt = 160;
 let resultado = calcularIMC("","");
 console.log(`O IMC de ${kg,alt} é ${resultado}`);
